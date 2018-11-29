d548c05cc5208208ff18d5c52b228627fed88f5a
Renato <6089803+renatoch@users.noreply.github.com>
2018-11-29 00:33:13 -0200

    refactor - clareza

--- KataWordWrap/Wrapper.cs
+++ KataWordWrap/Wrapper.cs
@@ -9,39 +9,39 @@ namespace KataWordWrap
             var result = new StringBuilder();
 
             var stringSize = input.Length;
-            if (stringSize > columns) {
-
-                var remaining = input;
-
-                do {
-                    var part = remaining.Substring(0, columns);
-
-                    remaining = remaining.Substring(columns);
-
-
-                    var isPartFullSentence = remaining[0] == ' ';
-                    if (isPartFullSentence) {
-                        result.AppendLine(part);
-                        remaining = remaining.Substring(1);
-                    } else {
-                        var lastSpaceIndex = part.LastIndexOf(" ", StringComparison.Ordinal);
-                        if (lastSpaceIndex <= -1) {
-                            result.AppendLine(part);
-                        } else {
-                            result.AppendLine(part.Substring(0, lastSpaceIndex));
-
-                            remaining = part.Substring(lastSpaceIndex + 1) + remaining;
-                        }
-                    }
-
-                } while (remaining.Length > columns);
-
-                result.Append(remaining);
-
-                return result.ToString().Replace("\r", "");
+            if (stringSize <= columns) {
+                return input;
             }
 
-            return input;
+            var remaining = input;
+
+            do {
+                var part = remaining.Substring(0, columns);
+
+                remaining = remaining.Substring(columns);
+
+
+                var isPartFullSentence = remaining[0] == ' ';
+                if (isPartFullSentence) {
+                    result.AppendLine(part);
+                    remaining = remaining.Substring(1);
+                } else {
+                    var lastSpaceIndex = part.LastIndexOf(" ", StringComparison.Ordinal);
+                    if (lastSpaceIndex <= -1) {
+                        result.AppendLine(part);
+                    } else {
+                        result.AppendLine(part.Substring(0, lastSpaceIndex));
+
+                        remaining = part.Substring(lastSpaceIndex + 1) + remaining;
+                    }
+                }
+
+            } while (remaining.Length > columns);
+
+            result.Append(remaining);
+
+            return result.ToString().Replace("\r", "");
+
         }
     }
 }

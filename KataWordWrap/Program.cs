46d3b1158b98a216fb4b1d72313cd8e8b5dc5c32
Renato <6089803+renatoch@users.noreply.github.com>
2018-11-29 00:33:28 -0200

    Melhoria na saída do console para saber onde fica o final das colunas

--- KataWordWrap/Program.cs
+++ KataWordWrap/Program.cs
@@ -5,8 +5,10 @@ namespace KataWordWrap
     class Program
     {
         static void Main(string[] args) {
+            var columns = 15;
+            Console.WriteLine(new String(' ', columns - 1) + "|");
             var texto = "Hoje é domingo, pede cachimbo. Cachimbo é de ouro. Bate no touro.";
-            var wrap = Wrapper.Wrap(texto, 14);
+            var wrap = Wrapper.Wrap(texto, columns);
             Console.WriteLine(wrap);
         }
     }
